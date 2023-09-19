cea01ea1555038df0be2633bdf50b6a15f8cfb60
neo9955 <neos13326682931@gmail.com>
2023-09-20 01:35:54 +1000

    Added 4 blank components

--- Cyber-Learner/src/app/app.module.ts
+++ Cyber-Learner/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PhishingComponent } from './phishing/phishing.component';
+import { PasswordComponent } from './password/password.component';
+import { TeamworkComponent } from './teamwork/teamwork.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PhishingComponent,
+    PasswordComponent,
+    TeamworkComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
