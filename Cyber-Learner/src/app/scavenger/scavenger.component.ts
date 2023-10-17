9d6662f77872fcbdd6efe690e53a6e47eed02fc8
Kyle <kyle.anders12@outlook.com>
2023-10-17 23:30:11 +1000

    Fixed bugs in scavenger hunt

--- Cyber-Learner/src/app/scavenger/scavenger.component.ts
+++ Cyber-Learner/src/app/scavenger/scavenger.component.ts
@@ -22,15 +22,33 @@ export class ScavengerComponent implements OnInit {
     );
   }
 
+  // Checks the answers in JSON file against user input
   checkAnswer(index: number) {
     const question = this.questions[index];
-    question.userAnswer = question.userAnswer.trim().toLowerCase();
-    const correctAnswers = question.answers.map((answer: string) => answer.toLowerCase());
+    question.userAnswer = question.userAnswer.trim().toString().toLowerCase();
 
-    if (correctAnswers.includes(question.userAnswer)) {
-      console.log(`Question ${index + 1}: Correct`);
+    if (question.answers && Array.isArray(question.answers)) {
+      const correctAnswers = question.answers.map((answer: string) => answer.toString().toLowerCase());
+  
+      // Checks if answers are included in the questions array of answers
+      if (correctAnswers.includes(question.userAnswer)) {
+        console.log(`Question ${index + 1}: Correct`);
+      } else {
+        // This line is only necessary when debugging
+        // console.log(`Question ${index + 1}: Incorrect`);
+      }
     } else {
-      console.log(`Question ${index + 1}: Incorrect`);
+      console.log(`Question ${index + 1}: No correct answers defined`);
+      console.log(question.answers);
     }
+  }
+
+  // Provides class for dynamic CSS of answer section
+  isAnswerCorrect(index: number): boolean {
+    const question = this.questions[index];
+    const userAnswer = question.userAnswer? question.userAnswer.trim().toLowerCase() : '';
+    const correctAnswers = question.answers.map((answer: string) => answer.toLowerCase());
+  
+    return correctAnswers.includes(userAnswer);
   }
 }
